be9cf2962e7f4f7a0fb84397e09920185c8185a9
Diatemba99 <58808297+Diatemba99@users.noreply.github.com>
2021-08-20 13:08:24 +0000

    Add files via upload

---
+++ tuples.py
@@ -0,0 +1,12 @@
+xaliss = ("bitcoin", "etherum", "dollars", "francs")
+
+print(xaliss)
+
+print(xaliss[2])
+
+print("**************************")
+print(len(xaliss))
+
+print("**************************")
+xalissBess = tuple("ECO")
+print(xalissBess)
