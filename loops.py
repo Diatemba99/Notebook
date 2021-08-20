e27514c55896557ef6c7bdb8440c5d451992fa63
Diatemba99 <58808297+Diatemba99@users.noreply.github.com>
2021-08-20 13:07:03 +0000

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,35 @@
+Mylist=["data", "deep", "learning", "machine"]
+
+for x in Mylist:
+    print(x)
+"""
+print("Using of range fonction part 1:")
+
+for b in range(5):
+    print(b)
+
+print("Using of range fonction part 2 with explaining starting and ending:")
+for c in range(3,15):
+    print("\t",c)    
+
+print("Using of range fonction part 3 with explaining starting,ending and step:")
+for d in range(0,155,5):
+    print("\t"*3,d)        
+"""   
+
+print('Starting "while" loop interations')
+
+counter = 1
+while(counter <= 17):
+    print(counter)
+    counter +=1 
+
+print('Starting "while" loop interations part2:')
+
+counter2 = 0
+while(counter2 < 17):
+    print("yangui thi bir fils")
+    counter2 +=1  
+else:
+    print("yangui thi biti fils")       
+
