c565e264252187831e1780dc8c2bf95f8ebddf6b
Diatemba99 <58808297+Diatemba99@users.noreply.github.com>
2021-08-20 13:08:08 +0000

    Add files via upload

---
+++ starting.py
@@ -0,0 +1,28 @@
+#Variables déclaration
+"""
+
+tin=15
+dia=25
+
+print("The sum is", tin+dia)
+print("The difference is", tin-dia)
+print("The modulo is", tin%dia)
+print("The product is", tin*dia)
+print("The quotient is", tin/dia)
+print("The power is", tin**dia)
+print("The second quotient is", tin//dia)
+
+#String operations
+
+elobe = "NiSaFOuL"
+
+print(elobe.upper())
+print(elobe.lower())
+mystring="welcome to coding world"
+print(mystring[len(mystring)-1])
+
+(5 != 7) == (2 >1)
+"""
+
+
+print((1!=1)==(4>5)) 
